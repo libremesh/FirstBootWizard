059618c741c99850a754c717c6e2e3283345b1b6
luandro <luandro@gmail.com>
2018-09-19 14:07:33 -0300

    working search_networks and create_network

--- first-boot-wizard-daemon/files/usr/lib/lua/firstbootwizard.lua
+++ first-boot-wizard-daemon/files/usr/lib/lua/firstbootwizard.lua
@@ -62,12 +62,14 @@ end
 
 function get_networks()
     local allRadios = wireless.scandevices()
+    nixio.syslog("crit", "FBW radios "..json.encode(allRadios))
     local all_networks = {}
     local phys = {}
     for _, radio in pairs (allRadios) do
-        if wireless.is5Ghz(radio[".name"]) then
-            phys[#phys+1] = "phy"..radio[".index"]
-        end
+        -- if wireless.is5Ghz(radio[".name"]) then
+            local phyIndex = radio[".name"].sub(radio[".name"], -1)
+            phys[#phys+1] = "phy"..phyIndex
+        -- end
     end
     nixio.syslog("crit", "FBW phys"..json.encode(phys))
     for idx, phy in pairs(phys) do
@@ -116,7 +118,8 @@ function connect(mesh_network)
     uci_cursor:set("wireless", device_name, "bssid", 'ca:fe:00:c0:ff:ee')
     uci_cursor:set("wireless", device_name, "mcast_rate", '24000')
 
-    uci_cursor:commit("wireless")
+    --10uci_c then   if ()
+    -- endursor:commit("wireless")
 
     -- apply wifi config
     execute("wifi down; wifi up;")
@@ -124,11 +127,13 @@ end
 
 function fetch_config(host)
     local filename = "/tmp/lime-defaults-"..host
+    nixio.syslog("crit", "FBW fetch config "..filename)
     execute("wget http://["..host.."]/lime-defaults -O "..filename.." 2>&1")
     return file_exists(filename) and filename or nil
 end
 
 function get_stations_macs(network)
+    nixio.syslog("crit", "FBW get_stations_macs "..network)
     return lsplit(execute('iw dev '..network..' station dump | grep ^Station | cut -d\\  -f 2'))
 end
 
@@ -152,16 +157,29 @@ function read_configs()
     return result
 end
 
+local function tableEmpty(self)
+    for _, _ in pairs(self) do
+        return false
+    end
+    return true
+end
+
 function get_config(mesh_network)
     connect(mesh_network)
+    local mode = mesh_network.mode == "Mesh Point" and 'mesh' or 'adhoc'
+    local dev_id = 'wlan'..mesh_network['phy_idx']..'-'..mode
     -- time for the mesh to settle
-    execute("sleep 2")
-
-    local dev_id = 'wlan'..mesh_network['phy_idx']..'-mesh'
+    -- TODO: check if connected if not sleep some more
+    local isAssociated = iwinfo.nl80211.assoclist(dev_id)
+    local i = 0
+    while (tableEmpty(isAssociated)) and i < 10 do
+        execute("sleep 1")
+        isAssociated = iwinfo.nl80211.assoclist(dev_id)
+        i = i + 1
+    end
     local stations = get_stations_macs(dev_id)
     local append_network = ft.curry(function (s1, s2) return s2..'%'..s1 end, 2) (dev_id)
     local hosts = ft.map(append_network, ft.map(eui64, stations))
-
     configs = ft.map(fetch_config, hosts)
     return ft.filter(function(el) return el ~= nil end, configs)
 end
@@ -234,7 +252,22 @@ function check_lock_file()
         return false
     end
     return true
-end    
+end
+
+function remove_lock_file()
+    execute("rm /etc/first_run")
+end
+
+function apply_user_configs(configs)
+    local ssid = configs.ssid
+    local uci_cursor = uci.cursor()
+    print(ssid)
+    uci_cursor:set("lime-defaults", 'wifi', 'ap_ssid', ssid)
+    uci_cursor:commit("lime-defaults")
+    -- apply wifi config
+    execute("wifi down; wifi up;")
+    return { configs = configs }
+end
 
 function get_all_networks()
     start_scan_file()
